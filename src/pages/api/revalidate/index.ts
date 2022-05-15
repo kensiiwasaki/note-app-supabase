c493e4469dfdd2a24e68a5311c1722f3e1a5ad7b
けんし <kensiiwasaki@gmail.com>
2022-05-15 10:41:34 +0900

    update

--- src/pages/api/revalidate/index.ts
+++ src/pages/api/revalidate/index.ts
@@ -3,12 +3,18 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 type Data = {
   revalidated: boolean
 }
+type Msg = {
+  message: string
+}
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data | Msg>
 ) {
   console.log('Recalidating notes page...')
+  if (req.query.secret !== process.env.REVALDATE_SECRET) {
+    return res.status(401).json({ message: 'Your secret id invalid !' })
+  }
   let revalidated = false
 
   try {
